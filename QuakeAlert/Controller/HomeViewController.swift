82a28ae0107c03633e37f8c1e80f770cfc2d58d1
Jackson Wang <jw@Jacksons-iMac.attlocal.net>
2019-07-10 02:48:56 -0700

    first push

---
+++ QuakeAlert/Controller/HomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  QuakeAlert
+//
+//  Created by Jackson Wang on 7/10/19.
+//  Copyright © 2019 Jackson Wang. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    /**
+     *   LAST UPDATED: 7/10
+    **/
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    func setupView() {
+        // set up navigation items
+        let barRightItemImage = UIImage(named: "setting")
+        let barLeftItemImage = UIImage(named: "map")
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: barRightItemImage, style: .plain, target: self, action: #selector(goToSetting))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: barLeftItemImage, style: .plain, target: self, action: #selector(goToMap))
+    }
+    
+    @objc func goToSetting() {
+        // push to setting view controller
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let settingViewController  = storyBoard.instantiateViewController(withIdentifier: "SettingViewController")
+        self.present(settingViewController, animated: true, completion: nil)
+    }
+    
+    @objc func goToMap() {
+        // push to map view controller
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let mapViewController = storyBoard.instantiateViewController(withIdentifier: "MapViewController")
+        self.present(mapViewController, animated: true, completion: nil)
+        
+    }
+}
+
